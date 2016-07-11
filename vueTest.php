eb734e99f353ec56b47cd52439b15089ceea5b2d
MudSeeYou <mm930202@gmail.com>
2016-07-11 15:52:51 +0800

    vueTest.php
    just a test using Vue.js

---
+++ vueTest.php
@@ -0,0 +1,89 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="vue.js"></script>
+    <script>
+        window.onload=function(){
+            var one = new Vue({
+                el:'.tBox'
+                ,data:{
+                    alert:'hello to everyone'
+                    ,shows: [
+                        { text: 'Learn JavaScript' },
+                        { text: 'Learn Vue.js' },
+                        { text: 'Build Something Awesome' }
+                    ]
+                }
+                ,methods:{
+                    toEnter:function(){
+                        var tempText = this.alert;
+                        if(tempText){
+                            this.shows.push({text:tempText});
+                            this.alert = '';
+                        }
+                    }
+                    ,toRemove:function(index){
+                        this.shows.splice(index,1);
+                    }
+                }
+            });
+            var three = new Vue({
+                el: '#app',
+                data: {
+
+                }
+            })
+            // 这是我们的 Model
+            var exampleData = {
+                name: 'Vue.js'
+            }
+
+                // 创建一个 Vue 实例或 "ViewModel"
+                // 它连接 View 与 Model
+            var exampleVM = new Vue({
+                el: '#example-1',
+                data: exampleData
+            })
+        }
+    </script>
+    <style>
+        a{
+            color:#0075c2;
+        }
+        a:hover{
+            color:#ccc;
+        }
+        .test{
+            background-color: #fed22f;
+            height:200px;
+            width: 20px;
+        }
+        .testB{
+            background-color: #c683fe;
+            height:20px;
+            width: 20px;
+        }
+    </style>
+</head>
+<body >
+<div class="tBox">
+    <p>{{alert}}</p>
+    <input  v-model="alert" v-on:keyup.enter="toEnter">
+    <ul>
+        <li v-for="show in shows">
+            <span>{{show.text}}</span>
+            <button v-on:click="toRemove($index)">XXX</button>
+        </li>
+    </ul>
+
+</div>
+<!-- 这是我们的 View -->
+<div id="example-1">
+    Hello {{ name }}!
+</div>
+
+
+</body>
+</html>
